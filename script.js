7a536f25bc4cd5c348545d00e709b2253c6ac626
jorgenmyrvold <jorgeum@stud.ntnu.no>
2021-09-03 16:38:13 +0200

    More or less finished master

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const DT = 0.1; // Speed of circles
+const SVG_HEIGHT_WIDTH = 100;
+const CANVAS_HEIGHT_WIDTH = 1000;
+const CANVAS_CIRCLE_RADIUS = 100;
+const CANVAS_MAX_SPEED = 40;
+const NUM_CIRCLES = 6;
+const SVG_CIRCLE_RADIUS = 5;
+const SVG_MAX_SPEED = 5;
+
+let randomHue = [180, 180, 180, 180, 180];
+let posAngle = 0;
+
+let requestAnimationFrame =
+  window.requestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.msRequestAnimationFrame;
+
 // For showing and hiding documentation in accordion using jQuery
 $(".accordion-header").click(function () {
   $(this).toggleClass("active");
@@ -54,70 +72,181 @@ function HSLToHex(h, s, l) {
   return "#" + r + g + b;
 }
 
-let requestAnimationFrame =
-  window.requestAnimationFrame ||
-  window.mozRequestAnimationFrame ||
-  window.webkitRequestAnimationFrame ||
-  window.msRequestAnimationFrame;
-
-let svgCircle = document.getElementById("svg-circle");
 let svgCircles = [];
-let randomHue = 180;
-let posAngle = 0;
 
-svgCircle.addEventListener("click", function () {
-  randomHue = Math.random() * 360;
-});
+function svgCirclesInit(svgCircles) {
+  const maxXY = SVG_HEIGHT_WIDTH - SVG_CIRCLE_RADIUS;
+  const minXY = SVG_CIRCLE_RADIUS;
+
+  for (let i = 0; i < NUM_CIRCLES; i++) {
+    let newCircle = {
+      id: i,
+      radius: SVG_CIRCLE_RADIUS,
+      x: Math.floor(Math.random() * (maxXY - minXY + 1) + minXY),
+      y: Math.floor(Math.random() * (maxXY - minXY + 1) + minXY),
+      vx: -SVG_MAX_SPEED + Math.random() * 2 * SVG_MAX_SPEED,
+      vy: -SVG_MAX_SPEED + Math.random() * 2 * SVG_MAX_SPEED,
+      fill: HSLToHex(Math.random() * 360, 100, 50),
+    };
+    svgCircles.push(newCircle);
+  }
+}
+svgCirclesInit(svgCircles);
+
+function createSVGCircles(item, index, array) {
+  const svgns = "http://www.w3.org/2000/svg";
+  const svgWrapper = document.getElementById("svg-animation");
+  const newCircle = document.createElementNS(svgns, "circle");
+
+  newCircle.setAttribute("id", "svg-circle" + item.id);
+  newCircle.setAttribute("cx", item.x.toString());
+  newCircle.setAttribute("cy", item.y.toString());
+  newCircle.setAttribute("r", item.radius.toString());
+  newCircle.setAttribute("fill", item.fill);
+  svgWrapper.append(newCircle);
+}
+svgCircles.forEach(createSVGCircles);
+
+function changeRandomHue(item, index, array) {
+  const circle = document.getElementById("svg-circle" + item.id);
+  circle.addEventListener("click", function () {
+    item.fill = HSLToHex(Math.random() * 360, 100, 50);
+  });
+}
+svgCircles.forEach(changeRandomHue);
+
+function collisionWithWall(circle, wallX, wallY) {
+  // On the form circle1 = {radius: 20, x: 5, y: 5, vx: 5, vy: 5};
+  if (circle.x <= 0 + circle.radius || circle.x >= wallX - circle.radius) {
+    circle.vx = -circle.vx;
+  }
+
+  if (circle.y <= 0 + circle.radius || circle.y >= wallY - circle.radius) {
+    circle.vy = -circle.vy;
+  }
+}
+
+function collisionDetection(circle1, circle2) {
+  // On the form circle1 = {radius: int, x: int, y: int, ...};
+
+  var dx = circle1.x - circle2.x;
+  var dy = circle1.y - circle2.y;
+  var distance = Math.sqrt(dx * dx + dy * dy);
+
+  if (distance < circle1.radius + circle2.radius) {
+    // collision detected!
+  }
+}
+
+function drawSVGCircles(item, index, array) {
+  let htmlCircle = document.getElementById("svg-circle" + item.id);
+
+  item.x += item.vx * DT;
+  item.y += item.vy * DT;
+
+  htmlCircle.setAttribute("cx", item.x.toString());
+  htmlCircle.setAttribute("cy", item.y.toString());
+  htmlCircle.setAttribute("fill", item.fill);
+}
+svgCircles.forEach(drawSVGCircles);
 
 function animageSVG() {
-  let svgAnimation = document.getElementById("svg-animation");
-  let svgCircle = document.getElementById("svg-circle");
-  svgAnimation.style.fill = HSLToHex(randomHue, 100, 50);
+  for (let i = 0; i < svgCircles.length; i++) {
+    let htmlCircle = document.getElementById("svg-circle" + svgCircles[i].id);
 
-  posAngle += Math.PI / 100;
-  svgCircle.setAttribute("cx", 50 + Math.cos(posAngle) * 25);
-  svgCircle.setAttribute("cy", 50 + Math.sin(posAngle) * 25);
+    svgCircles[i].x += svgCircles[i].vx * DT;
+    svgCircles[i].y += svgCircles[i].vy * DT;
 
+    collisionWithWall(svgCircles[i], SVG_HEIGHT_WIDTH, SVG_HEIGHT_WIDTH);
+
+    htmlCircle.setAttribute("cx", svgCircles[i].x.toString());
+    htmlCircle.setAttribute("cy", svgCircles[i].y.toString());
+    htmlCircle.setAttribute("fill", svgCircles[i].fill);
+  }
   requestAnimationFrame(animageSVG);
 }
 animageSVG();
 
-// HTML canvas script
-const CANVAS_WIDTH_HEIGHT = 1000;
+// ---------------------- HTML canvas script --------------------------
 const canvas = document.getElementById("canvas-animation");
 
-canvas.width = CANVAS_WIDTH_HEIGHT;
-canvas.height = CANVAS_WIDTH_HEIGHT;
+canvas.width = CANVAS_HEIGHT_WIDTH;
+canvas.height = CANVAS_HEIGHT_WIDTH;
 
 const ctx = canvas.getContext("2d");
+let canvasCircles = [];
 
-let fillColor = "hsl(180, 100%, 50%)";
-let radiusAngle = 0;
+function canvasCirclesInit(canvasCircles) {
+  const maxXY = CANVAS_HEIGHT_WIDTH - CANVAS_CIRCLE_RADIUS;
+  const minXY = CANVAS_CIRCLE_RADIUS;
+
+  for (let i = 0; i < NUM_CIRCLES; i++) {
+    let newCircle = {
+      id: i,
+      radius: CANVAS_CIRCLE_RADIUS,
+      x: Math.floor(Math.random() * (maxXY - minXY + 1) + minXY),
+      y: Math.floor(Math.random() * (maxXY - minXY + 1) + minXY),
+      vx: -CANVAS_MAX_SPEED + Math.random() * 2 * CANVAS_MAX_SPEED,
+      vy: -CANVAS_MAX_SPEED + Math.random() * 2 * CANVAS_MAX_SPEED,
+      fill: HSLToHex(Math.random() * 360, 100, 50),
+    };
+    canvasCircles.push(newCircle);
+  }
+}
+canvasCirclesInit(canvasCircles);
 
 // Draws and animates the circle
 function draw() {
-  ctx.clearRect(0, 0, CANVAS_WIDTH_HEIGHT, CANVAS_WIDTH_HEIGHT);
-  ctx.fillStyle = fillColor;
+  ctx.clearRect(0, 0, CANVAS_HEIGHT_WIDTH, CANVAS_HEIGHT_WIDTH);
 
-  radiusAngle += Math.PI / 70; // Alter this to alter speed of animation
-  let radius = 150 + Math.cos(radiusAngle) * 100;
-  let posX =
-    CANVAS_WIDTH_HEIGHT / 2 + (Math.cos(posAngle) * CANVAS_WIDTH_HEIGHT) / 4;
-  let posY =
-    CANVAS_WIDTH_HEIGHT / 2 + (Math.sin(posAngle) * CANVAS_WIDTH_HEIGHT) / 4;
+  for (let i = 0; i < canvasCircles.length; i++) {
+    canvasCircles[i].x += canvasCircles[i].vx * DT;
+    canvasCircles[i].y += canvasCircles[i].vy * DT;
 
-  ctx.beginPath();
-  ctx.arc(posX, posY, radius, 0, 2 * Math.PI);
-  ctx.closePath();
+    collisionWithWall(
+      canvasCircles[i],
+      CANVAS_HEIGHT_WIDTH,
+      CANVAS_HEIGHT_WIDTH
+    );
 
-  ctx.fill();
+    ctx.beginPath();
+    ctx.fillStyle = canvasCircles[i].fill;
+    ctx.arc(
+      canvasCircles[i].x,
+      canvasCircles[i].y,
+      canvasCircles[i].radius,
+      0,
+      2 * Math.PI
+    );
+    ctx.fill();
+  }
+
   requestAnimationFrame(draw);
 }
 draw();
 
-// Changes color of the circle on the canvas when clicked
-canvas.addEventListener("click", function () {
-  fillColor = "hsl(" + Math.random() * 360 + ", 100%, 50%)";
+function getMousePos(evt) {
+  var rect = canvas.getBoundingClientRect(), // abs. size of element
+    scaleX = canvas.width / rect.width, // relationship bitmap vs. element for X
+    scaleY = canvas.height / rect.height; // relationship bitmap vs. element for Y
+
+  const clickX = (evt.clientX - rect.left) * scaleX;
+  const clickY = (evt.clientY - rect.top) * scaleY;
+
+  for (let i = 0; i < canvasCircles.length; i++) {
+    if (
+      clickX < canvasCircles[i].x + canvasCircles[i].radius &&
+      clickX > canvasCircles[i].x - canvasCircles[i].radius &&
+      clickY < canvasCircles[i].y + canvasCircles[i].radius &&
+      clickY > canvasCircles[i].y - canvasCircles[i].radius
+    ) {
+      canvasCircles[i].fill = HSLToHex(Math.random() * 360, 100, 50);
+    }
+  }
+}
+
+canvas.addEventListener("click", function (e) {
+  getMousePos(e);
 });
 
 // Scaling SVG to parent
